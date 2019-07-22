d1bf5cab2f9458b4b4f4484f85a6944df81cb932
alex_crow <alexcrowdnepr@gmail.com>
2019-07-22 23:14:30 +0300

    test

--- src/test/java/ua/crow/home/service/impl/ExcelPOIHelperServiceImplTest.java
+++ src/test/java/ua/crow/home/service/impl/ExcelPOIHelperServiceImplTest.java
@@ -1,11 +1,16 @@
 package ua.crow.home.service.impl;
 
+import org.apache.poi.ss.usermodel.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import ua.crow.home.service.ExcelPOIHelperService;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,6 +22,16 @@ class ExcelPOIHelperServiceImplTest {
 
     @Test
     void writeExcel() throws IOException {
-        excelPOIHelperService.writeExcel();
+        Workbook wb = WorkbookFactory.create(excelPOIHelperService.writeExcel());
+        Sheet sheet = wb.getSheetAt(0);
+        Map<Integer, List<String>> data = new HashMap<>();
+        int i = 0;
+        for (Row row : sheet) {
+            data.put(i, new ArrayList<String>());
+            for (Cell cell : row) {
+                System.out.println(cell.getStringCellValue());
+            }
+            i++;
+        }
     }
 }
